8d5fe4fe529f6ce0a9b689ea3fe9a2571d071f02
simont-org <68000215+simont-org@users.noreply.github.com>
2020-12-21 09:48:46 +0100

    Add files via upload

---
+++ edit_playlist.php
@@ -0,0 +1,19 @@
+<?php
+$fn = "/home/pi/WoodStream/playlist.txt";
+$file = fopen($fn, "r+") or die("Unable to open file!");
+$size = filesize($fn);
+
+if($_POST['allfile']) {
+  fwrite($file, $_POST['allfile']);
+  rewind($file);
+}
+
+$text = fread($file, $size);
+fclose($file);
+?>
+
+<form action="<?=$PHP_SELF?>" method="post">
+	<textarea COLS="120" ROWS="30" name="allfile"><?=$text?></textarea>
+	<br/>
+	<button type="submit">Update</button>
+</form>
